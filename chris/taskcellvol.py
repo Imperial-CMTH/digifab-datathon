7be37fffd20a6a18ee0a428e5747cbe54e76d07b
Chris <christopher.bradley@live.co.uk>
2021-03-24 15:48:08 +0000

    abcdef

--- chris/taskcellvol.py
+++ chris/taskcellvol.py
@@ -63,15 +63,3 @@ plt.show()
 # ).to_numpy()
 # test_pred = pclf.predict(test_data)
 # # np.savetxt('task_cellvol_predictions.csv', test_pred)
-
-
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-
-
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
-
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
